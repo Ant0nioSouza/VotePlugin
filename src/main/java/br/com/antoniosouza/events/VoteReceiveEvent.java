bc37bf1085cf778102b3d9b352a863e8d3262038
Antonio Souza <antoniosouza.dev@gmail.com>
2021-11-17 12:49:11 -0300

    feat(events): Event sends kit to player

--- src/main/java/br/com/antoniosouza/events/VoteReceiveEvent.java
+++ src/main/java/br/com/antoniosouza/events/VoteReceiveEvent.java
@@ -1,6 +1,7 @@
 package br.com.antoniosouza.events;
 
 import com.vexsoftware.votifier.model.VotifierEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -9,6 +10,12 @@ public class VoteReceiveEvent implements Listener {
 
     @EventHandler(priority = EventPriority.HIGH)
     public void onVotifierEvent(VotifierEvent e) {
-        // stuff here
+        try {
+            if (Bukkit.getPlayer(e.getVote().getUsername()).isOnline()) {
+                Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "kit " + e.getVote().getUsername() + " Tools");
+            }
+        } catch (Exception ex) {
+            // Player is not online.
+        }
     }
 }
