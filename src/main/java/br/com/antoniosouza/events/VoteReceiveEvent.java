56c31f96d0a2fd1a319327cf2660cca2f18d961e
Antonio Souza <antoniosouza.dev@gmail.com>
2021-11-17 18:03:04 -0300

    feat: Set commands on vote

--- src/main/java/br/com/antoniosouza/events/VoteReceiveEvent.java
+++ src/main/java/br/com/antoniosouza/events/VoteReceiveEvent.java
@@ -1,18 +1,40 @@
 package br.com.antoniosouza.events;
 
+import br.com.antoniosouza.util.ConfigUtil;
 import com.vexsoftware.votifier.model.VotifierEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 public class VoteReceiveEvent implements Listener {
+    ConfigUtil configUtil = new ConfigUtil();
 
     @EventHandler(priority = EventPriority.HIGH)
     public void onVotifierEvent(VotifierEvent e) {
+        Player player = Bukkit.getPlayer(e.getVote().getUsername());
         try {
-            if (Bukkit.getPlayer(e.getVote().getUsername()).isOnline()) {
-                Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "kit " + e.getVote().getUsername() + " Tools");
+            if (player.isOnline()) {
+                player.sendMessage(configUtil.getMessage());
+                for (String command : configUtil.getCommands()) {
+                    String consoleCommand = "";
+                    String[] splitedCommand = command.split(" ");
+                    String[] treatedCommand = splitedCommand;
+                    for (int i = 0; i < splitedCommand.length; i++) {
+                        if (treatedCommand[i].equals("USER")) {
+                            treatedCommand[i] = e.getVote().getUsername();
+                        }
+                    }
+                    for (int i = 0; i < treatedCommand.length; i ++) {
+                        if (i == 0) {
+                            consoleCommand += treatedCommand[i];
+                            continue;
+                        }
+                        consoleCommand += " " + treatedCommand[i];
+                    }
+                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), consoleCommand);
+                }
             }
         } catch (Exception ex) {
             // Player is not online.
