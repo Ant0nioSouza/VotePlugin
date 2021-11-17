fc569960551c56d4fcbf1c8a66a47fd5b38e5f8f
Antonio Souza <antoniosouza.dev@gmail.com>
2021-11-17 16:04:03 -0300

    fix: NullPointerException config file

--- src/main/java/br/com/antoniosouza/util/ConfigUtil.java
+++ src/main/java/br/com/antoniosouza/util/ConfigUtil.java
@@ -1,14 +1,20 @@
 package br.com.antoniosouza.util;
 
 import br.com.antoniosouza.VotePlugin;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
 import java.util.List;
 
 public class ConfigUtil {
-    JavaPlugin plugin = new VotePlugin();
-    FileConfiguration config = plugin.getConfig();
+
+    public ConfigUtil(FileConfiguration config) {
+        this.config = config;
+    }
+    FileConfiguration config;
 
     public String getMessage() {
         return config.getString("voteMessage");
