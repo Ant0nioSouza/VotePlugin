5c11bb3e206a858300af42379f9279cbead6634b
Antonio Souza <antoniosouza.dev@gmail.com>
2021-11-18 20:43:22 -0300

    fix: returning through the right index

--- src/main/java/br/com/antoniosouza/util/ConfigUtil.java
+++ src/main/java/br/com/antoniosouza/util/ConfigUtil.java
@@ -8,6 +8,7 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ConfigUtil {
@@ -23,7 +24,17 @@ public class ConfigUtil {
     public List<String> getCommands() {
         return config.getStringList("commands");
     }
-    public static List<String> getRandomItens() {
-        return config.getStringList("randomItensID");
+    public static List<String[][]> getRandomItens() {
+        List<String[][]> finalIQ;
+        finalIQ = new ArrayList();
+        List<String> rawIQ = config.getStringList("randomItensID");
+        String[][] itemQuantity = new String[rawIQ.size()][rawIQ.size()];
+        for (int i = 0; i < rawIQ.size(); i ++) {
+            String[] treatedIQ = rawIQ.get(i).split("/");
+            itemQuantity[i][0] = treatedIQ[0];
+            itemQuantity[i][1] = treatedIQ[1];
+            finalIQ.add(itemQuantity);
+        }
+        return finalIQ;
     }
 }
