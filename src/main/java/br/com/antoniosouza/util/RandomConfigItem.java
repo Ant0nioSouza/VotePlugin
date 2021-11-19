27655773c0f3165c13b628b38f43c033f0975ec0
Antonio Souza <antoniosouza.dev@gmail.com>
2021-11-19 09:12:29 -0300

    style: removed commented lines

--- src/main/java/br/com/antoniosouza/util/RandomConfigItem.java
+++ src/main/java/br/com/antoniosouza/util/RandomConfigItem.java
@@ -15,22 +15,16 @@ public class RandomConfigItem {
     public static ItemStack[] randomIten() {
         ItemStack[] itens = new ItemStack[ConfigUtil.getHowManyItens()];
         int randIndex;
-        System.out.println("======> " + rand.nextInt(ConfigUtil.getRandomItens().size()));
-
        for (int i = 0; i < ConfigUtil.getHowManyItens(); i++) {
             randIndex = rand.nextInt(ConfigUtil.getRandomItens().size());
 
            if (ConfigUtil.getRandomItens().get(randIndex)[randIndex][0].contains(":")) {
                 int id = Integer.parseInt(ConfigUtil.getRandomItens().get(randIndex)[randIndex][0].split(":")[0]);
                 int byteID = Integer.parseInt(ConfigUtil.getRandomItens().get(randIndex)[randIndex][0].split(":")[1]);
-               System.out.println("int | Byte | " + byteID + " | " + (byte)byteID);
                 Material material = Material.getMaterial(id);
-                System.out.println("Rand index -> " + randIndex + " | rand amount: " + ConfigUtil.getRandomItens().get(randIndex)[randIndex][1] + " | rand id: " + ConfigUtil.getRandomItens().get(randIndex)[randIndex][0]);
                 ItemStack item = new ItemStack(id, Integer.parseInt(ConfigUtil.getRandomItens().get(randIndex)[randIndex][1]), (short) byteID);
                 itens[i] = item;
-                System.out.println(itens[i]);
             } else {
-                System.out.println("Rand index -> " + randIndex + " | rand amount: " + ConfigUtil.getRandomItens().get(randIndex)[randIndex][1] + " | rand id: " + ConfigUtil.getRandomItens().get(randIndex)[randIndex][0]);
                 ItemStack item = new ItemStack(Material.getMaterial(Integer.parseInt(ConfigUtil.getRandomItens().get(randIndex)[randIndex][0])));
                 item.setAmount(Integer.parseInt(ConfigUtil.getRandomItens().get(randIndex)[randIndex][1]));
                 itens[i] = item;
