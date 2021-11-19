8d70a9d51696e7e23cc45b29628078ba960bc688
Antonio Souza <antoniosouza.dev@gmail.com>
2021-11-19 09:10:25 -0300

    feat(command): now give to player some random item

--- src/main/java/br/com/antoniosouza/commands/VoteCommands.java
+++ src/main/java/br/com/antoniosouza/commands/VoteCommands.java
@@ -1,16 +1,31 @@
 package br.com.antoniosouza.commands;
 
 import br.com.antoniosouza.util.ConfigUtil;
+import br.com.antoniosouza.util.RandomConfigItem;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class VoteCommands implements CommandExecutor {
     ConfigUtil cu = new ConfigUtil();
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         sender.sendMessage(ChatColor.RED + "Vote: " + cu.getWebsite());
+        ItemStack[] itens = RandomConfigItem.randomIten();
+        if (ConfigUtil.isRandom()) {
+            for (ItemStack item : itens) {
+                if (Bukkit.getServer().getOnlinePlayers() != null) {
+                    for (Player p : Bukkit.getServer().getOnlinePlayers()) {
+                        p.getInventory().addItem(item);
+                    }
+                }
+            }
+        }
+
         return true;
     }
 }
